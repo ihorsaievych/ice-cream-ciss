93c290637429763c2940d1934ac70b181e349a68
ihorsaievych <55617503+ihorsaievych@users.noreply.github.com>
2022-07-24 08:20:56 +0000

    Deploying to gh-pages from @ ihorsaievych/ice-cream-CISS@1fc3eadbedb992fa659e17ea4c2a341979601966 🚀

--- index.b7d8ca02.js
+++ index.b7d8ca02.js
@@ -1,2 +1,2 @@
 var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",type:"bullets"}});
-//# sourceMappingURL=index.a08ab160.js.map
+//# sourceMappingURL=index.b7d8ca02.js.map
