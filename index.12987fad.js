5c1134f5e5eccce2ea4158c371b8a8ea663302aa
ihorsaievych <55617503+ihorsaievych@users.noreply.github.com>
2022-07-26 14:17:19 +0000

    Deploying to gh-pages from @ ihorsaievych/ice-cream-ciss@30c39cdc8c8d40e0721bb5f5eba7342ce0a5464e 🚀

---
+++ index.12987fad.js
@@ -0,0 +1,2 @@
+jQuery((function(){var e=jQuery("#gallery");jQuery("picture",e).hover((function(){e.find("picture").removeClass("big"),jQuery(this).addClass("big")}))}));
+//# sourceMappingURL=index.12987fad.js.map
