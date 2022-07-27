450db39959034653f24c100f5bcf4df8fb3aa634
ihorsaievych <55617503+ihorsaievych@users.noreply.github.com>
2022-07-27 19:20:05 +0000

    Deploying to gh-pages from @ ihorsaievych/ice-cream-ciss@c399f8b12a423eb42c98940b63aabd7a420bd559 🚀

---
+++ index.f5a4e427.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},simulateTouch:!0,grabCursor:!0,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},mousewheel:{sensitivity:1},initialSlide:2,speed:800});
+//# sourceMappingURL=index.f5a4e427.js.map
