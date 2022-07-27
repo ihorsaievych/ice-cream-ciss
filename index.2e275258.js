d16b278f33a245cb13e1c2b9f8e6dea0a004e255
ihorsaievych <55617503+ihorsaievych@users.noreply.github.com>
2022-07-27 04:48:53 +0000

    Deploying to gh-pages from @ ihorsaievych/ice-cream-ciss@8053e6ca63a5b13f0fd1579b24c99d5eb7af85ea 🚀

---
+++ index.2e275258.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0},simulateTouch:!0,grabCursor:!0,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},mousewheel:{sensitivity:1},initialSlide:2,autoplay:{delay:3e3,disableOnInteraction:!1},speed:800});
+//# sourceMappingURL=index.2e275258.js.map
--- index.d3104978.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},simulateTouch:!0,grabCursor:!0,spaceBetween:10});
-//# sourceMappingURL=index.d3104978.js.map
