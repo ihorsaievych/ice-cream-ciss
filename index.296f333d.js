be6ff798630217ef02bad44160e60e9be11b83b3
ihorsaievych <55617503+ihorsaievych@users.noreply.github.com>
2022-07-28 17:51:41 +0000

    Deploying to gh-pages from @ ihorsaievych/ice-cream-ciss@93370357c51c6fe61a64c3595e1bc4b2a4e7f813 🚀

---
+++ index.296f333d.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){var e=document.querySelector(".hero-header-fixed");window.pageYOffset>400?e.classList.add("header_fixed"):e.classList.remove("header_fixed")};
+//# sourceMappingURL=index.296f333d.js.map
